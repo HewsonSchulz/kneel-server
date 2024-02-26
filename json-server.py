9eef2729c5549c583b35928b6095caae41ae9dce
Hewson Schulz <hewsonschulz@gmail.com>
2024-02-26 17:55:55 -0600

    Create New Order lesson

--- json-server.py
+++ json-server.py
@@ -1,6 +1,7 @@
+import json
 from http.server import HTTPServer
 from nss_handler import HandleRequests, status
-from views import get_all_orders, get_single_order
+from views import get_all_orders, get_single_order, create_order
 
 
 class JSONServer(HandleRequests):
@@ -25,6 +26,28 @@ class JSONServer(HandleRequests):
 
             response_body = get_all_orders()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
+
+        else:
+            return self.response(
+                "", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
+
+    def do_POST(self):
+        """Handle POST requests from a client"""
+
+        url = self.parse_url(self.path)
+
+        if url["requested_resource"] == "orders":
+            content_len = int(self.headers.get("content-length", 0))
+            request_body = self.rfile.read(content_len)
+            request_body = json.loads(request_body)
+            order = create_order(request_body)
+            if order:
+                return self.response(
+                    json.dumps(order), status.HTTP_201_SUCCESS_CREATED.value
+                )
+            else:
+                return self.response("", status.HTTP_500_SERVER_ERROR.value)
 
         else:
             return self.response(
--- views/order_view.py
+++ views/order_view.py
@@ -52,3 +52,45 @@ def get_single_order(pk):
         query_results = db_cursor.fetchone()
 
     return json.dumps(dict(query_results)) if query_results else None
+
+
+def create_order(data):
+    with sqlite3.connect(database) as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            INSERT INTO Orders
+            (styleId, sizeId, metalId, typeId)
+            VALUES (?, ?, ?, ?)
+            """,
+            (data["styleId"], data["sizeId"], data["metalId"], data["typeId"]),
+        )
+
+        order_id = db_cursor.lastrowid
+
+        # retrieve the newly created order
+        db_cursor.execute(
+            """
+            SELECT
+                o.id,
+                o.styleId,
+                o.sizeId,
+                o.metalId,
+                o.typeId
+            FROM Orders o
+            WHERE o.id = ?
+            """,
+            (order_id,),
+        )
+        new_order = db_cursor.fetchone()
+
+        order_dict = {
+            "id": new_order[0],
+            "styleId": new_order[1],
+            "sizeId": new_order[2],
+            "metalId": new_order[3],
+            "typeId": new_order[4],
+        }
+
+    return order_dict if order_dict else None
